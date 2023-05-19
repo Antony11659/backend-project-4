0cd7d76a4d059937cf7b3f9889d2f3e36a4b0890
ANT_SHUR <shurupov.tosha93@gmail.com>
2023-05-19 19:51:51 +0300

    step#3 part1

--- src/load.js
+++ src/load.js
@@ -3,34 +3,62 @@ import axios from 'axios';
 import path from 'path';
 import process from 'process';
 import * as cheerio from 'cheerio';
+import nock from 'nock';
 
-const buildName = (file, type) => {
-  const [, addressWithoutProtocol] = file.split('//');
+const testURL = 'https://ru.hexlet.io/courses';
+nock(testURL).get('').reply(200, async () => {
+  const data = await fs.promises.readFile('../__fixtures__/before.html', 'utf-8');
+  return data;
+});
+
+const isSrcLocal = (url, src) => {
+  const domain = new URL(url).hostname;
+  const srcDomain = new URL(src, url).hostname;
+  return domain === srcDomain;
+};
+
+const buildName = (file, type = '') => {
+  const addressWithoutProtocol = file.replace(/^https?:\/\//, '');
   const result = addressWithoutProtocol.split(/[./]/g).join('-');
   return result.concat(type);
 };
-
-const generateImageName = (url) => 'ru-hexlet-io-assets-professions-nodejs.png'; 
 
 const loadUrl = (url, newUrl, localPath) => new Promise((resolve) => {
   const data = axios.get(url, { responseType: 'stream' });
   resolve(data);
 }).then((response) => {
   const imageFile = path.join(localPath, newUrl);
-  fs.promises.writeFile(imageFile, response.data);
+  // fs.promises.writeFile(imageFile, response.data);
 })
   .catch((err) => new Error(err));
 
-const downloadImage = (data, localPath) => {
+const downloadImage = (domain, data, localPath) => {
   const $ = cheerio.load(data);
+  // $('img').each(function () {
+  //   const oldSrc = $(this).attr('src');
+  //   // const newSrc = path.join(localPath, generateImageName(oldSrc));
+  //   const newSrc = '/assets/professions/nodejs.png';
+  //   loadUrl(oldSrc, generateImageName(oldSrc), localPath); // newSrc should be here
+  //   $(this).attr('src', newSrc);
+  // });
+
+  // SELECT ALL RESOURCES
   $('img').each(function () {
     const oldSrc = $(this).attr('src');
-    // const newSrc = path.join(localPath, generateImageName(oldSrc));
-    const newSrc = '/assets/professions/nodejs.png';
-    loadUrl(oldSrc, generateImageName(oldSrc), localPath); // newSrc should be here
+    const newSrc = isSrcLocal(domain, oldSrc) ? buildName(oldSrc, '.png') : oldSrc;
     $(this).attr('src', newSrc);
   });
-  return $.html();
+  $('script').each(function () {
+    const oldSrc = $(this).attr('src');
+    const newSrc = isSrcLocal(domain, oldSrc) ? buildName(oldSrc, '.js') : oldSrc;
+    $(this).attr('src', newSrc);
+  });
+  $('link').each(function () {
+    const oldSrc = $(this).attr('href');
+    const newSrc = isSrcLocal(domain, oldSrc) ? buildName(oldSrc, '.css') : oldSrc;
+    $(this).attr('href', newSrc);
+  });
+  console.log($.html());
 };
 
 const downloadPage = (url, dir = process.cwd()) => {
@@ -39,19 +67,27 @@ const downloadPage = (url, dir = process.cwd()) => {
 
   return new Promise((resolve) => {
     const data = axios.get(url);
-    fs.promises.mkdir(imageDir);
+    // fs.promises.mkdir(imageDir);
     resolve(data);
   })
-    .then((response) => downloadImage(response.data, imageDir))
-    .then((response) => {
-      fs.promises.writeFile(filePath, response);
-      console.log(filePath);
-      return filePath;
-    })
+    // .then((response) => downloadImage(response.data, imageDir))
+    .then((response) => downloadImage(url, response.data, imageDir))
+    // .then((response) => {
+  // console.log(response);
+  // fs.promises.writeFile(filePath, response);
+  // console.log(filePath);
+  // return filePath;
+    // })
     .catch((err) => {
       throw new Error(err.message);
     });
 };
 
-// downloadPage('https://ru.hexlet.io/courses ');
-export default downloadPage;
+downloadPage(testURL);
+// export default downloadPage;
+
+// 1) <link href="/courses" rel="canonical">  =>  <link href="-courses.css" rel="canonical">
+//  something is wrong with generating the name 
+// (should be <link href="ru-hexlet-io-courses_files/ru-hexlet-io-courses.html" rel="canonical">)
+// 2) To load all resources
+// 3) make tests
