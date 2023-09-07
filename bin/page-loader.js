fd91693d54680de2b240158a311a78d363beb026
ANT_SHUR <shurupov.tosha93@gmail.com>
2023-09-07 20:26:32 +0300

    correct bin/page-loader.js

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -2,7 +2,6 @@
 import { program } from 'commander';
 import debug from 'debug';
 import pageLoad from '../src/load.js';
-import handleError from '../src/errors/errorHandler.js';
 
 program
   .name('Usage: page-loader [options] <url>')
@@ -10,16 +9,19 @@ program
   .version('1.0.0')
   .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir"')
   .option('-d, --debug', 'enable debug console')
-  .action(() => new Promise((resolve) => {
+  .action(() => {
     const dir = program.opts().output;
     if (program.opts().debug) {
       debug.enable('page-loader, axios');
     }
     const url = program.args[0];
-    resolve(pageLoad(url, dir));
+    return pageLoad(url, dir)
+      .then((res) => {
+        console.log(`Page was successfully downloaded into ${res}`);
+      })
+      .catch((error) => {
+        console.error(`${error.message} while downloading the page ${url}`);
+        process.exit(1);
+      });
   })
-    .catch((err) => {
-      handleError(err);
-      process.exit(1);
-    }))
   .parse(process.argv);
